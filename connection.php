7de87c2a6128006a9193e65b7a3295392887363c
SimZin-17 <simzen.madonsela@gmail.com>
2026-06-23 20:19:54 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,13 @@
 <?php
-$host = "localhost";
-$dbname = "popcart";
-$username = "root";
-$password = "";
-$port = 4306;
+$host = "sql103.infinityfree.com";
+$dbname = "if0_42224709_PopCart";
+$username = "if0_42224709";
+$password = "rkh9GE3IKh";
 
 try {
-  $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
   die("Connection failed: " . $e->getMessage());
 }
 ?>
-
